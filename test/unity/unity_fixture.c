d291a435481a96c51896704e8c47408b158a46b4
Alexander Klauer <Alexander.Klauer@itwm.fraunhofer.de>
2012-12-07 16:24:09 +0100

    Unity update

--- test/unity/unity_fixture.c
+++ test/unity/unity_fixture.c
@@ -163,7 +163,7 @@ void UnityMalloc_MakeMallocFailAfterCount(int countdown)
 
 typedef struct GuardBytes
 {
-    int size;
+    size_t size;
     char guard[sizeof(int)];
 } Guard;
 
@@ -184,12 +184,15 @@ void * unity_malloc(size_t size)
 
     malloc_count++;
 
-    guard = (Guard*)malloc(size + sizeof(Guard) + 4);
+    guard = malloc( size + sizeof(Guard) + strlen( end ) + 1 );
+    if ( guard == NULL ) {
+	    return NULL;
+    }
     guard->size = size;
     mem = (char*)&(guard[1]);
     memcpy(&mem[size], end, strlen(end) + 1);
 
-    return (void*)mem;
+    return mem;
 }
 
 static int isOverrun(void * mem)
