55efd3adab240cd7deb98acc280731ccbf46bcae
Alexander Klauer <Alexander.Klauer@itwm.fraunhofer.de>
2015-08-14 17:41:08 +0200

    Added support for local timestamps

--- sml/include/sml/sml_time.h
+++ sml/include/sml/sml_time.h
@@ -28,16 +28,28 @@ extern "C" {
 
 #define SML_TIME_SEC_INDEX 0x01
 #define SML_TIME_TIMESTAMP 0x02
+#define SML_TIME_TIMESTAMP_LOCAL 0x03
+
+typedef struct {
+	u32 * timestamp;
+	i16 * local_offset;
+	i16 * season_time_offset;
+} sml_timestamp_local;
 
 typedef struct {
 	u8 *tag;
 	union {
 		u32 *sec_index;
 		u32 *timestamp;
+		sml_timestamp_local * local_timestamp;
 	}
 	data;
 } sml_time;
 
+sml_timestamp_local * sml_timestamp_local_init( u32 timestamp, i16 local_offset, i16 season_time_offset );
+sml_timestamp_local * sml_timestamp_local_parse( sml_buffer * buf );
+int sml_timestamp_local_write( sml_timestamp_local * timestamp_local, sml_buffer * buf );
+void sml_timestamp_local_free( sml_timestamp_local * timestamp_local );
 sml_time *sml_time_init();
 sml_time *sml_time_parse(sml_buffer *buf);
 void sml_time_write(sml_time *time, sml_buffer *buf);
--- sml/src/sml_time.c
+++ sml/src/sml_time.c
@@ -22,6 +22,94 @@
 #include <sml/sml_number.h>
 #include <stdio.h>
 
+sml_timestamp_local * sml_timestamp_local_init( u32 timestamp, i16 local_offset, i16 season_time_offset ) {
+	sml_timestamp_local * t = malloc( sizeof( *t ) );
+	if ( t != NULL ) {
+		*t = ( sml_timestamp_local ) {
+			.timestamp = sml_u32_init( timestamp ),
+			.local_offset = sml_i16_init( local_offset ),
+			.season_time_offset = sml_i16_init( season_time_offset )
+		};
+		if ( ( t->timestamp == NULL ) || ( t->local_offset == NULL ) || ( t->season_time_offset == NULL ) ) {
+			sml_timestamp_local_free( t );
+			t = NULL;
+		}
+	}
+
+	return t;
+}
+
+int sml_timestamp_local_write( sml_timestamp_local * t, sml_buffer * buf ) {
+	if ( t == NULL ) {
+		sml_buf_optional_write( buf );
+		return 0;
+	}
+
+	sml_buf_set_type_and_length( buf, SML_TYPE_LIST, 3 );
+	sml_u32_write( t->timestamp, buf );
+	sml_i16_write( t->local_offset, buf );
+	sml_i16_write( t->season_time_offset, buf );
+
+	return 0;
+}
+
+sml_timestamp_local * sml_timestamp_local_parse( sml_buffer * buf ) {
+	sml_timestamp_local * result = NULL;
+
+	if ( sml_buf_optional_is_skipped( buf ) ) {
+		return NULL;
+	}
+
+	if ( sml_buf_get_next_type( buf ) != SML_TYPE_LIST ) {
+		buf->error = 1;
+		goto error;
+	}
+
+	if ( sml_buf_get_next_length( buf ) != 3 ) {
+		buf->error = 1;
+		goto error;
+	}
+
+	result = malloc( sizeof( *result ) );
+	if ( result == NULL ) {
+		buf->error = 1; /* only way to signal errors */
+		goto error;
+	}
+	result->timestamp = NULL;
+	result->local_offset = NULL;
+	result->season_time_offset = NULL;
+
+	result->timestamp = sml_u32_parse( buf );
+	if ( sml_buf_has_errors( buf ) ) {
+		goto error;
+	}
+
+	result->local_offset = sml_i16_parse( buf );
+	if ( sml_buf_has_errors( buf ) ) {
+		goto error;
+	}
+
+	result->season_time_offset = sml_i16_parse( buf );
+	if ( sml_buf_has_errors( buf ) ) {
+		goto error;
+	}
+
+	return result;
+
+error:
+	sml_timestamp_local_free( result );
+	return NULL;
+}
+
+void sml_timestamp_local_free(sml_timestamp_local *t) {
+	if ( t != NULL ) {
+		free( t->timestamp );
+		free( t->local_offset );
+		free( t->season_time_offset );
+		free( t );
+	}
+}
+
 sml_time *sml_time_init() {
 	sml_time *t = (sml_time *) malloc(sizeof(sml_time));
 	*t = ( sml_time ) {
@@ -51,7 +139,23 @@ sml_time *sml_time_parse(sml_buffer *buf) {
 	tme->tag = sml_u8_parse(buf);
 	if (sml_buf_has_errors(buf)) goto error;
 
-	tme->data.timestamp = sml_u32_parse(buf);
+	switch ( *tme->tag ) {
+		case SML_TIME_SEC_INDEX:
+			tme->data.sec_index = sml_u32_parse( buf );
+			break;
+
+		case SML_TIME_TIMESTAMP:
+			tme->data.timestamp = sml_u32_parse( buf );
+			break;
+
+		case SML_TIME_TIMESTAMP_LOCAL:
+			tme->data.local_timestamp = sml_timestamp_local_parse( buf );
+			break;
+
+		default:
+			buf->error = 1;
+			break;
+	}
 	if (sml_buf_has_errors(buf)) goto error;
 
 	return tme;
@@ -69,14 +173,39 @@ void sml_time_write(sml_time *t, sml_buffer *buf) {
 
 	sml_buf_set_type_and_length(buf, SML_TYPE_LIST, 2);
 	sml_u8_write(t->tag, buf);
-	sml_u32_write(t->data.timestamp, buf);
+	switch( *t->tag ) {
+		case SML_TIME_SEC_INDEX:
+			sml_u32_write( t->data.sec_index, buf );
+			break;
+
+		case SML_TIME_TIMESTAMP:
+			sml_u32_write( t->data.timestamp, buf );
+			break;
+
+		case SML_TIME_TIMESTAMP_LOCAL:
+			sml_timestamp_local_write( t->data.local_timestamp, buf );
+			break;
+	}
 }
 
 void sml_time_free(sml_time *tme) {
     if (tme) {
+	    if ( tme->tag ) {
+		switch ( *tme->tag ) {
+			case SML_TIME_SEC_INDEX:
+				sml_number_free( tme->data.sec_index );
+				break;
+
+			case SML_TIME_TIMESTAMP:
+				sml_number_free( tme->data.timestamp );
+				break;
+
+			case SML_TIME_TIMESTAMP_LOCAL:
+				sml_timestamp_local_free( tme->data.local_timestamp );
+				break;
+		}
 		sml_number_free(tme->tag);
-		sml_number_free(tme->data.timestamp);
+	    }
         free(tme);
     }
 }
-
